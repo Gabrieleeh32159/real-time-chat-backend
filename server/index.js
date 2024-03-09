5a6e3a0dc7f569ca24af856b6b4e3facddaaa8ba
gabrieleeh32159 <gabriel.espinoza.h@utec.edu.pe>
2024-03-09 01:53:30 -0500

    Adding start script

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = Express()
 const server = createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173"
+        origin: process.env.NODE_ENV === 'production' ? undefined : "http://localhost:5173"
     }
 })
 
