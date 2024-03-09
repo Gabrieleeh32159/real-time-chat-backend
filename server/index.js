7f875877c346ed62d96d26d463b7837b46968058
gabrieleeh32159 <gabriel.espinoza.h@utec.edu.pe>
2024-03-09 02:03:30 -0500

    Fix adding front production url to cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = Express()
 const server = createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: process.env.NODE_ENV === 'production' ? undefined : "http://localhost:5173"
+        origin: process.env.NODE_ENV === 'production' ? "https://real-time-chat-production-2aff.up.railway.app" : "http://localhost:5173"
     }
 })
 
